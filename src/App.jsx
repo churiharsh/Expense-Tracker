66edc83f3a26c39364a0e2a447786748d69de612
Harsh Churi <harsh.201904111@vcet.edu.in>
2022-09-29 11:51:20 +0530

    Google not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import { useEffect, useState } from 'react'
-import Signin from './components/Signin/Signin'
+import Signin from './components/Signin/Signin';
+import Dashboard from './components/Dashboard/Dashboard';
+
+
 // import './App.css'
 import { gapi } from 'gapi-script';
-const clientId='949936098006-hblta500v948358n1htrm7qnvlcaf49k.apps.googleusercontent.com';
+const clientId = '949936098006-hblta500v948358n1htrm7qnvlcaf49k.apps.googleusercontent.com';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  useEffect(()=>{
-    const initClient=()=>{
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const initClient = () => {
       gapi.client.init({
-        clientId:clientId,
-        scope:''
+        clientId: clientId,
+        scope: ''
       })
     };
-    gapi.load('client:auth2',initClient)
-  
+    gapi.load('client:auth2', initClient)
+
   });
   return (
-    
-    <div >
-      <Signin/>
-    </div>
+    <Signin/>
+
   )
 }
 
